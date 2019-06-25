2328053b9630a181fe5a8e7cefafd75ea75d3529
Carol <947752894@qq.com>
2019-06-25 17:27:55 +0800

    Update RedisConfig.java
    修改了redis使用模型，可以配合哨兵和集群模式使用redis

--- common/src/main/java/site/cnkj/util/config/RedisConfig.java
+++ common/src/main/java/site/cnkj/util/config/RedisConfig.java
@@ -1,26 +1,37 @@
 package site.cnkj.util.config;
 
+import cn.migu.log.utils.RedisUtil;
+import cn.migu.log.utils.domain.redis.Receiver;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import site.cnkj.util.RedisUtil;
 
-
+/*
+ * @version 1.0 created by LXW on 2019/6/25 10:30
+ */
 @Configuration
-@ConfigurationProperties
 public class RedisConfig {
 
-    @Value("${spring.application.name}")
+    @Value("${spring.application.name:''}")
     private String redisName = "";
 
     @Bean
     public RedisTemplate<String, Object> functionDomainRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
         return redisTemplate;
     }
@@ -28,9 +39,9 @@ public class RedisConfig {
     private void initDomainRedisTemplate(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory factory) {
         //如果不配置Serializer，那么存储的时候缺省使用String，如果用User类型存储，那么会提示错误User can't cast to String！
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         // 开启事务
         redisTemplate.setEnableTransactionSupport(true);
         redisTemplate.setConnectionFactory(factory);
@@ -42,6 +53,33 @@ public class RedisConfig {
         redisUtil.setRedisTemplate(redisTemplate);
         redisUtil.setRedisName(redisName);
         return redisUtil;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.redis.subDescription", name = "channel")
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter, @Value("${spring.redis.subDescription.channel}") String channel) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic(channel));
+        return container;
+    }
+
+    /**
+     * 消息监听器适配器，绑定消息处理器，利用反射技术调用消息处理器的业务方法
+     * 需要做多少个消息通信就新建多少个bean并且绑定不同的defaultListenerMethod，可以使用同一个component
+     * @param receiver
+     * @return
+     */
+    @Bean
+    @ConditionalOnBean(Receiver.class)
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiver");
+    }
+
+    /**redis 读取内容的template */
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
     }
 
 }
